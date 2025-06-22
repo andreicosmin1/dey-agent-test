266afd7eebd41090eef28651c3cc90b559654c5c
andreicosmin1 <andreidezmiream@gmail.com>
2025-06-22 16:28:50 +0300

    Update test_flow_data.py

--- test_flow_data.py
+++ test_flow_data.py
@@ -1,1 +1,11 @@
+# test_flow_data.py
+
+from modules import flow_data
+
+# Tokenul tău Unusual Whales
+token = "TOKENUL_TAU"
+
+score, message = flow_data.run(token)
+print("Scor:", score)
+print("Interpretare:", message)
 
